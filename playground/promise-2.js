25f9bfa31970dbf575057ae3485fef3944dcefa1
Abinash Anand <abinashanandab@gmail.com>
2024-01-04 05:52:50 +0530

    routing created for all users and tasks

--- playground/promise-2.js
+++ playground/promise-2.js
@@ -1,10 +1,22 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
-Task.findByIdAndDelete('65952b952cbee735519c0c97').then((task)=>{
-    console.log(task);
-    return Task.countDocuments({completed:true})
-}).then((result)=>{
-    console.log(result);
+// Task.findByIdAndDelete('65952b952cbee735519c0c97').then((task)=>{
+//     console.log(task);
+//     return Task.countDocuments({completed:true})
+// }).then((result)=>{
+//     console.log(result);
+// }).catch((e)=>{
+//     console.log(e);
+// })
+
+
+const deleteTaskAndCount = async (id)=>{
+    const deleteTaskById = await Task.findByIdAndDelete(id);
+    const countDocument = await Task.countDocuments({completed:true}) 
+    return {deleteTaskById,countDocument};
+}
+deleteTaskAndCount('65952a0b7532fbb15732bd5b').then((result)=>{
+    console.log("result",result);
 }).catch((e)=>{
     console.log(e);
 })
--- src/index.js
+++ src/index.js
@@ -1,54 +1,154 @@
 const express = require('express')
 require('./db/mongoose')
 const user = require('./models/user')
-const task = require('./models/task')
+const task = require('./models/task');
+
 const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json())
 
-//user route POST request
-app.post('/user',(req, res)=>{
-    const userOne  = new user(req.body)
-    userOne.save().then((result)=>{
-        res.send(userOne)
-    }).catch((error)=>{
-        res.send(error)
-    })
-})
-
-
-
-//task route POST request
-app.post('/task', (req, res)=>{
-    const newTask = new task(req.body);
-    newTask.save().then((result)=>{
-        res.send(newTask)
-    }).catch((error)=>{
-
-        res.status(400).send(error)
-        })
-})
-
-//task route to GET data  find all method
-app.get('/task', (req, res)=>{
+//user route POST request {CREATING}
+app.post('/user', async (req, res)=>{
    
-    task.find({}).then((task)=>{
-        res.send(task)
-    }).catch((e)=>{
-        res.send(e)
-    })
+    try {
+        const userOne  = new user(req.body)
+        await userOne.save()
+        res.status(201).send(userOne)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
 })
-//task route to GET data findById method
-app.get('/task/:id', (req, res)=>{
-    const id = req.params.id;
-    task.findById(id).then((task)=>{
-     if(!task){
-        return res.status(404).send()
+
+//task route to GET data find() method {READING ALLL USERS}
+app.get('/user', async (req, res)=>{
+    try {
+          const userFinder = await user.find({})
+          res.status(200).send(userFinder)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  
+  })
+
+
+  
+//task route to GET data findById method {READING SINGLE USSER}
+app.get('/user/:id',async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const singleUser =  await user.findById(id)
+        if(!singleUser){
+            return 'User not found!'
+        }
+        res.status(200).send(singleUser)
+        
+    } catch (error) {
+     res.status(500).send(error)   
+    }
+})
+//USER ROUTE TO UPDATE A USER BY PATCH() {UPDATING A SINGLE USER}
+app.patch('/user/:id', async (req, res)=>{
+    try {
+     const updateUser =  await user.findByIdAndUpdate(req.params.id,req.body,{new:true, runValidators:true})
+     if(!updateUser){
+      return res.status(404).send()
      }
-        res.send(task)
-    }).catch((e)=>{
-        res.status(500).send(e)
-    })
+     res.status(200).send(updateUser);
+
+    } catch (error) {
+      res.status(400).send(error)
+    }
+    
+}) 
+    //SELETE METHOD {DELETING A SINGLE USER }
+    app.delete('/user/:id', async (req, res) => {
+        try {
+          const deletedUser = await user.findByIdAndDelete(req.params.id);
+          if (!deletedUser) {
+            return res.status(404).send();
+          }
+          res.status(200).send(deletedUser);
+        } catch (error) {
+          res.status(400).send(error); // Corrected to send the 'error' variable
+        }
+      });
+      
+
+//task route POST request {CREATING A TASK}
+app.post('/task', async (req, res)=>{
+    try {
+        const newTask =  new task(req.body);
+        await  newTask.save()
+        res.status(201).send(newTask);
+
+
+    } catch (error) {
+            res.status(400).send(error)
+    }
+
+})
+
+//task route to GET data  find all method {READING ALL TASKS}
+app.get('/task', async (req, res)=>{
+  try {
+        const taskFinder = await task.find({})
+        res.status(200).send(taskFinder)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+
+})
+
+
+//task route to GET data findById method {READING A SINGLE TASK}
+app.get('/task/:id',async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const singleTask =  await task.findById(id)
+        if(!singleTask){
+            return 'Task not found!'
+        }
+        res.status(200).send(singleTask)
+        
+    } catch (error) {
+     res.status(500).send(error)   
+    }
+
+ 
+
+//TASK ROUTE TO UPDATE A TASK BY PATCH() {UPDATING A SINGLE TASK}
+
+    app.patch('/task/:id', async (req, res)=>{
+        try {
+         const updateTask =  await task.findByIdAndUpdate(req.params.id,req.body,{new:true, runValidators:true})
+         if(!updateTask){
+          return res.status(404).send()
+         }
+         res.status(200).send(updateTask);
+  
+        } catch (error) {
+          res.status(400).send(error)
+        }
+        
+      }) 
+
+
+//TASK ROUTE TO DELETE A TASK BY PATCH() {DELETING A SINGLE TASK}
+app.delete('/task/:id', async (req, res)=>{
+    try {
+        const deleteOneTask =  await task.findByIdAndDelete(req.params.id);
+        if(!deleteOneTask){
+          return res.status(404).send('Task not found')
+        }
+        res.status(200).send(deleteOneTask)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+ 
+})
+
+
 
 })
 app.listen(port, ()=>{
